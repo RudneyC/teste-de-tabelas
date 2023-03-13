f757dfde1537f543f7717d16a4e661904e357f04
RudneyC <127165811+RudneyC@users.noreply.github.com>
2023-03-12 21:20:06 -0300

    Update script.js
    $(document).ready(function() {
        // Define a URL do arquivo CSV
        var url = "https://drive.google.com/uc?export=download&id=1VSTWJ_yrZLJcqNmt11bJPRTPNjOgBZCp";
    
        // Carrega o arquivo CSV com o AJAX
        $.get(url, function(data) {
            // Converte o arquivo CSV em um objeto JSON
            var jsonData = $.csv.toObjects(data);
    
            // Cria o cabeçalho da tabela com base nos nomes das colunas do JSON
            var tableHead = '<tr>';
            $.each(jsonData[0], function(key, value) {
                tableHead += '<th>' + key + '</th>';
            });
            tableHead += '</tr>';
            $('#table-head').append(tableHead);
    
            // Cria o corpo da tabela com base nos dados do JSON
            var tableBody = '';
            $.each(jsonData, function(index, value) {
                tableBody += '<tr>';
                $.each(value, function(key, value) {
                    tableBody += '<td>' + value + '</td>';
                });
                tableBody += '</tr>';
            });
            $('#table-body').append(tableBody);
    
            // Inicializa o plugin DataTables
            $('#myTable').DataTable({
                order: [[0, 'asc']],
                initComplete: function () {
                    // Adiciona filtros dinâmicos
                    this.api().columns().every(function () {
                        var column = this;
                        var select = $('<select><option value=""></option></select>')
                            .appendTo($(column.header()))
                            .on('change', function () {
                                var val = $.fn.dataTable.util.escapeRegex(
                                    $(this).val()
                                );
                                column
                                    .search(val ? '^' + val + '$' : '', true, false)
                                    .draw();
                            });
                        column.data().unique().sort().each(function (d, j) {
                            select.append('<option value="' + d + '">' +

--- script.js
+++ script.js
@@ -1,47 +1,55 @@
 $(document).ready(function() {
-    // Define a URL do arquivo CSV
-    var url = "https://drive.google.com/uc?export=download&id=1VSTWJ_yrZLJcqNmt11bJPRTPNjOgBZCp";
+	// Define a URL do arquivo CSV https://drive.google.com/file/d/1pqHt4-sgd52ijrAcSqx-zu_oz2nhkDXT/view?usp=sharing
+	var url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRJoRCDiKcFFH1KKy9aF99ohL-3KsYU8w6-rZzvKj1hHHnF6u8W6M9TNvQ2jovL-7fEa6rgmz6O8O6w/pub?output=csv";
 
-    // Carrega o arquivo CSV com o AJAX
-    $.get(url, function(data) {
-        // Converte o arquivo CSV em um objeto JSON
-        var jsonData = $.csv.toObjects(data);
+	// Carrega o arquivo CSV com o AJAX
+	$.get(url, function(data) {
+		// Converte o arquivo CSV em um objeto JSON
+		var jsonData = $.csv.toObjects(data);
 
-        // Cria o cabeçalho da tabela com base nos nomes das colunas do JSON
-        var tableHead = '<tr>';
-        $.each(jsonData[0], function(key, value) {
-            tableHead += '<th>' + key + '</th>';
-        });
-        tableHead += '</tr>';
-        $('#table-head').append(tableHead);
+		// Obtém os cabeçalhos das colunas
+		var headers = Object.keys(jsonData[0]);
 
-        // Cria o corpo da tabela com base nos dados do JSON
-        var tableBody = '';
-        $.each(jsonData, function(index, value) {
-            tableBody += '<tr>';
-            $.each(value, function(key, value) {
-                tableBody += '<td>' + value + '</td>';
-            });
-            tableBody += '</tr>';
-        });
-        $('#table-body').append(tableBody);
+		// Adiciona os cabeçalhos das colunas na tabela
+		var tableHead = $('#table-head');
+		var headRow = $('<tr></tr>');
+		for (var i = 0; i < headers.length; i++) {
+			headRow.append($('<th>' + headers[i] + '</th>'));
+		}
+		tableHead.append(headRow);
 
-        // Inicializa o plugin DataTables
-        $('#myTable').DataTable({
-            order: [[0, 'asc']],
-            initComplete: function () {
-                // Adiciona filtros dinâmicos
-                this.api().columns().every(function () {
-                    var column = this;
-                    var select = $('<select><option value=""></option></select>')
-                        .appendTo($(column.header()))
-                        .on('change', function () {
-                            var val = $.fn.dataTable.util.escapeRegex(
-                                $(this).val()
-                            );
-                            column
-                                .search(val ? '^' + val + '$' : '', true, false)
-                                .draw();
-                        });
-                    column.data().unique().sort().each(function (d, j) {
-                        select.append('<option value="' + d + '">' +
+		// Adiciona os dados na tabela
+		var tableBody = $('#table-body');
+		for (var i = 0; i < jsonData.length; i++) {
+			var bodyRow = $('<tr></tr>');
+			for (var j = 0; j < headers.length; j++) {
+				bodyRow.append($('<td>' + jsonData[i][headers[j]] + '</td>'));
+			}
+			tableBody.append(bodyRow);
+		}
+
+		// Inicializa a tabela com a biblioteca DataTables
+		var table = $('#table').DataTable({
+			order: [[0, 'asc']],
+			initComplete: function () {
+				// Adiciona filtros dinâmicos
+				this.api().columns().every(function () {
+					var column = this;
+					var select = $('<select><option value=""></option></select>')
+						.appendTo($(column.header()))
+						.on('change', function () {
+							var val = $.fn.dataTable.util.escapeRegex(
+								$(this).val()
+							);
+							column
+								.search(val ? '^' + val + '$' : '', true, false)
+								.draw();
+						});
+					column.data().unique().sort().each(function (d, j) {
+						select.append($('<option value="' + d + '">' + d + '</option>'));
+					});
+				});
+			}
+		});
+	});
+});
