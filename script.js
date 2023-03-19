b3aaff9f39435033e479f10ee1cc9cad80a7dd1a
RudneyC <127165811+RudneyC@users.noreply.github.com>
2023-03-18 22:04:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 $(document).ready(function() {
-  $('#tabela').DataTable({
-    "ajax": {
-      "url": "https://raw.githubusercontent.com/RudneyC/teste-de-tabelas/main/dados.csv",
-      "dataType": "csv",
-      "success": function(data) {
-        var parsedData = Papa.parse(data, {header: true}).data;
-        var table = $('#tabela').DataTable();
-        table.clear();
-        table.rows.add(parsedData);
-        table.draw();
+  $.ajax({
+    url: "https://raw.githubusercontent.com/RudneyC/teste-de-tabelas/main/dados.csv",
+    dataType: "text",
+    success: function(data) {
+      var headers = [];
+      var rows = [];
+
+      var allRows = data.split(/\r?\n|\r/);
+      headers = allRows[0].split(",");
+
+      for (var i = 1; i < allRows.length; i++) {
+        var dataRow = allRows[i].split(",");
+        if (dataRow.length == headers.length) {
+          rows.push(dataRow);
+        }
       }
+
+      var table = $('#tabela').DataTable({
+        data: rows,
+        columns: headers,
+        initComplete: function() {
+          var columnFilters = $("#tabela tfoot th").map(function() {
+            return this.innerHTML;
+          }).get();
+          this.api().columns().every(function(index) {
+            var column = this;
+            if (index < columnFilters.length) {
+              var filterType = columnFilters[index];
+              var select = $('<select><option value="">'+filterType+'</option></select>')
+                .appendTo($(column.footer()).empty())
+                .on('change', function() {
+                  var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                  column.search(val ? '^'+val+'$' : '', true, false).draw();
+                });
+              column.data().unique().sort().each(function (d, j) {
+                select.append('<option value="' + d + '">' + d + '</option>')
+              });
+            }
+          });
+        }
+      });
+    },
+    error: function(xhr, error) {
+      console.log(xhr);
+      console.log(error);
     }
   });
 });
