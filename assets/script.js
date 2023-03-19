363374eb8daa7d60ff55d21b067af9836c7d3b96
RudneyC <127165811+RudneyC@users.noreply.github.com>
2023-03-18 21:54:54 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,14 @@
 $(document).ready(function() {
-    $.ajax({
-        url: 'https://raw.githubusercontent.com/RudneyC/teste-de-tabelas/main/dados.csv',
-        dataType: 'text',
-    }).done(successFunction);
+  $('#tabela').DataTable( {
+      "ajax": {
+          "url": "https://raw.githubusercontent.com/RudneyC/teste-de-tabelas/main/dados.csv",
+          "dataSrc": ""
+      },
+      "columns": [
+          { "data": "Nome" },
+          { "data": "Sobrenome" },
+          { "data": "Idade" },
+          { "data": "Cidade" }
+      ]
+  } );
 });
-
-function successFunction(data) {
-    var allRows = data.split(/\r?\n|\r/);
-    var tableHead = '<tr>';
-    var tableBody = '';
-    for (var i = 0; i < allRows.length; i++) {
-        var rowCells = allRows[i].split(',');
-        if (i === 0) {
-            for (var j = 0; j < rowCells.length; j++) {
-                tableHead += '<th>' + rowCells[j] + '</th>';
-            }
-            tableHead += '</tr>';
-        } else {
-            tableBody += '<tr>';
-            for (var k = 0; k < rowCells.length; k++) {
-                tableBody += '<td>' + rowCells[k] + '</td>';
-            }
-            tableBody += '</tr>';
-        }
-    }
-    $('#table-head').html(tableHead);
-    $('#table-body').html(tableBody);
-    $('#myTable').DataTable();
-}
